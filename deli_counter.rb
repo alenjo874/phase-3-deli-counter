b1c6aa51bf962557174c0ee390ed47f622721cb6
Alen Jo <alenjo874@gmail.com>
2022-02-22 11:33:50 -0500

    commit tree 0fd89b3a94ff15349dffcee9da828f5594ce89f8

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
+require "pry"
 # Write your code here.
+def line(katz_deli)
+    line_pos = 0
+   
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else
+      new_arr = katz_deli.map do |person|
+                    "#{line_pos += 1}. #{person}"
+                end
+                puts "The line is currently: #{new_arr.join(" ")}"       
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+
+      if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"    
+      else 
+        puts  "Currently serving #{katz_deli.shift()}."
+      end
+      
+end
